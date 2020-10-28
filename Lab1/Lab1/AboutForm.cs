27380cad1bfe2fa364c37937a8db60c63a19ab69
Volodymyr Kovieshnikov <vladimirk17@gmail.com>
2020-10-28 11:17:41 +0200

    Lab1 and Lab2

---
+++ Lab1/Lab1/AboutForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.Title = "Awesome Console Application | Варіант 10";
+            Console.WriteLine("Welcome to the Awesome Console App!\nPress any key to continue...");
+            Console.ReadKey(true);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            #region Демонстрація Console.Read
+            //           var read = Console.Read();
+            //           Console.WriteLine(read);    
+            #endregion
+
+            while (true)
+            {
+                Console.Write("Enter 'r' to reset Console color...\n" +
+                              "Enter 'q' to exit the application\n");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "q":
+                        Environment.Exit(0);
+                        break;
+                    case "r":
+                        Console.ResetColor();
+                        Console.WriteLine("The color was successfully reset.");
+                        break;
+                    default:
+                        Console.WriteLine("Bad command. Try again.");
+                        Console.WriteLine();
+                        continue;
+                }
+            }
+        }
+	}
+}
